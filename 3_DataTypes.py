02128106810055e31a69e467338acb8cb1a3cc11
occototo <assi2411@github.com>
2023-04-30 10:52:10 -0400

    firstcommit

--- 3_DataTypes.py
+++ 3_DataTypes.py
@@ -1,8 +1,17 @@
 '''
-Primitive Datatypes
+--------------------
+PRIMITIVE DATATYPES
+--------------------
 Int 
 Float 
 String 
 Bool 
 None
 '''
+
+studentAge = 23                     # DataType, Int
+studentHeight = 5.9                 # DataType, Float
+stundentName = 'Aswin Bu'           # Datatype, String
+isIndian = True                     # Datatype, Bool
+isAmerican = False                  # Datatype, Bool
+haveDetails = None                  # DataType, None
